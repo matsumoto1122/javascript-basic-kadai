a5625e1be0fcb91e702a2bccdec8778653398ea9
matsumoto1122 <m.matsumoto.cms@gmail.com>
2025-09-12 13:37:21 +0900

    kadai_020

---
+++ kadai_020/event.js
@@ -0,0 +1,11 @@
+const bottonBtn = document.getElementById('btn');
+
+
+bottonBtn.addEventListener('click', () =>{
+
+    const textsBtn = document.getElementById('text');
+
+    textsBtn.textContent = 'ボタンをクリックしました';
+
+});
+
